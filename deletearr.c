e3e9fdd9435b0b55fb473577d101b9f7422ca178
gopal-codes2007 <gopaldasnaipur@gmail.com>
2025-04-10 22:22:45 +0530

    c programme

---
+++ deletearr.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int n, i, a;
+    printf("Enter the number of terms of the array: ");
+    scanf("%d", &n);
+    int arr[1000];
+    printf("Enter the elements of the array: \n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the number you want to delete from the Array :");
+    scanf("%d", &a);
+    for(i = 0; i < n; i++)
+    {
+        if(arr[i] == a)
+        {
+            for(int j = i; j < n; j++)
+            {
+                arr[j] = arr[j+1];
+            }
+            n--;
+        }
+    }
+    printf("Array after deletion is: \n");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+    return 0;
+}
