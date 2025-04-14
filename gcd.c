5ba213de3924ff287a2fd8a926f1ae60f77eb161
gopal-codes2007 <gopaldasnaipur@gmail.com>
2025-04-14 14:47:08 +0530

    c programme

---
+++ gcd.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,m;
+    printf("Enter first number : ");
+    scanf("%d", &n);
+    printf("Enter second number : ");
+    scanf("%d", &m);
+    while(n!=m)
+    {
+        if(n>m)
+        {
+            n=n-m;
+        }
+        else
+        {
+            m=m-n;
+        }
+    }
+    printf("\n GCD of two numbers is %d",n);
+    return 0;
+}
