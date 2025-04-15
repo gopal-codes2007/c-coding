6025fd1ea0c82b43673dca162e8025beda4b6c2f
gopal-codes2007 <gopaldasnaipur@gmail.com>
2025-04-15 12:34:05 +0530

    c programme

---
+++ calc.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum,sub,mul,div;
+    printf("enter first number : ");
+    scanf("%d", &a);
+    printf("%d", &b);
+    scanf("%d", &b);
+    sum=a+b;
+    sub=a-b;
+    mul=a*b;
+    div=a/b;
+    printf("The sum id =%d ,The sub is =%d , The mul is =%d , The div is = %d\n",sum,sub,mul,div);
+    return 0;
+}
