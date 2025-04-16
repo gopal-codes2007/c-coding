85313ba3765d5a23ab4365c0ebdd9671c4359147
gopal-codes2007 <gopaldasnaipur@gmail.com>
2025-04-16 18:28:31 +0530

    c programme

---
+++ reverse.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,r=0;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+    while(n>0);
+    {
+        r = r * 10;
+        r = r + (n % 10);
+        n = n / 10;
+    }
+    printf("The reverse number is %d",r);
+    return 0;
+}
