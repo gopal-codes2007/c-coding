8327331129e345d73eeb0b4f3c030242716582b7
gopal-codes2007 <gopaldasnaipur@gmail.com>
2025-04-23 08:19:19 +0530

    c programme

---
+++ sqrt.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a;
+    printf("Enter a number : ");
+    scanf("%d", &a);
+    int root = sqrt(a);
+    printf("The swuare of the i/p number is = %d",root);
+
+    return 0;
+}
